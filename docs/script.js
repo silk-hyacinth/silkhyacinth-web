87865f1069fd2d09e9727e07ee762e1aa58878d8
Benjamin Wang <benjamin.wang@duke.edu>
2026-08-23 01:06:37 -0400

    main screen update, fix stars, chnage bg color

--- docs/script.js
+++ docs/script.js
@@ -1,35 +1,11 @@
 function clickBody() {
-
-    console.log("test");
-    var div = document.createElement('div');
-    div.classList.add("shooting-star");
-    div.style.setProperty("--top-offset", Math.random()*80 + "vh")
-
-
-    document.getElementById("meteor-shower").appendChild(div);
-
-
-    var container_div = document.getElementById('meteor-shower');
-    var count = container_div.getElementsByClassName('star').length;
-
-    console.log(count)
-    if(count<20) {
-        for(let i = 0; i < 3; i++){
-            var newstar = document.createElement('div');
-            newstar.classList.add('star');
-            document.getElementById("meteor-shower").appendChild(newstar);
-        }
-    }
-    else {
-        setTimeout(d, 500)
-    }
-};
-
-function d() {
-    var container_div = document.getElementById('meteor-shower');
-    for (let i = 0; i < container_div.getElementsByClassName('star').length; i++) {
-        container_div.getElementsByClassName('star')[0].remove();
-    }
+    var star = document.createElement('div');
+    star.classList.add("shooting-star");
+    star.style.setProperty("--top-offset", Math.random() * 80 + "vh");
+    star.addEventListener("animationend", function() {
+        star.remove();
+    });
+    document.getElementById("meteor-shower").appendChild(star);
 }
 
 window.onload = function() {
