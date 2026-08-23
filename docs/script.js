397d238c99f0618cd05c1d6c33aa31d1a0f1b311
Benjamin Wang <benjamin.wang@duke.edu>
2026-08-23 02:56:48 -0400

    fixed more stuff, added projects page

--- docs/script.js
+++ docs/script.js
@@ -1,13 +1,34 @@
+var starsEnabled = true;
+
 function clickBody() {
+    if (!starsEnabled) {
+        return;
+    }
+
     var star = document.createElement('div');
     star.classList.add("shooting-star");
     star.style.setProperty("--top-offset", Math.random() * 80 + "vh");
-    star.addEventListener("animationend", function() {
+    star.addEventListener("animationend", function(event) {
+        if (event.animationName !== "fall") {
+            return;
+        }
         star.remove();
     });
     document.getElementById("meteor-shower").appendChild(star);
 }
 
+function toggleStars(event) {
+    event.stopPropagation();
+    starsEnabled = !starsEnabled;
+    document.getElementById("meteor-shower").classList.toggle("stars-off", !starsEnabled);
+    this.classList.toggle("off", !starsEnabled);
+}
+
 window.onload = function() {
     document.documentElement.addEventListener("click", clickBody);
+
+    var toggle = document.getElementById("stars-toggle");
+    if (toggle) {
+        toggle.addEventListener("click", toggleStars);
+    }
 }
